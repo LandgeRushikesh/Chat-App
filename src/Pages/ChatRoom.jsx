d944b6cb992446c6ce105c2383c4b162c6f57a6c
LandgeRushikesh <rushilandge12@gmail.com>
2025-07-23 19:20:33 +0530

    fixed scroll issue

--- src/Pages/ChatRoom.jsx
+++ src/Pages/ChatRoom.jsx
@@ -40,31 +40,41 @@ function ChatRoom() {
   };
 
   // Delete Message
-  const HandleDelete = async(message_id) =>{
-    let response = await databases.deleteDocument(config.AppWriteDatabaseId,config.AppWriteCollectionID,message_id)
-    console.log(response)
-    setMessages(messages.filter(message=>message.$id !== message_id))
-  }
+  const HandleDelete = async (message_id) => {
+    let response = await databases.deleteDocument(
+      config.AppWriteDatabaseId,
+      config.AppWriteCollectionID,
+      message_id
+    );
+    console.log(response);
+    setMessages(messages.filter((message) => message.$id !== message_id));
+  };
 
   return (
     <>
-      <div className="chat-room w-[50%] h-screen flex flex-col justify-start overflow-y-scroll">
-        <div className="w-full h-full flex flex-col justify-start "> 
+      <div className="chat-room w-[50%] h-screen flex flex-col justify-start">
+        <div className="chat w-full h-full flex flex-col justify-start overflow-y-scroll">
           {messages.map((message) => (
-          <div key={message.$id} className="w-full message my-4 p-4 flex justify-between items-center">
-            <div>
-              <p className="w-fit bg-rose-600 text-white font-semibold text-2xl py-3 px-4 rounded-xl">
-                {message.body}
-              </p>
-              <p className="text-gray-600 text-right">
-                {new Date(message.$updatedAt).toLocaleString()}
-              </p>
+            <div
+              key={message.$id}
+              className="w-full message my-4 p-4 flex justify-between items-center"
+            >
+              <div>
+                <p className="w-fit bg-rose-600 text-white font-semibold text-2xl py-3 px-4 rounded-xl">
+                  {message.body}
+                </p>
+                <p className="text-gray-600 text-right">
+                  {new Date(message.$updatedAt).toLocaleString()}
+                </p>
+              </div>
+              <button
+                onClick={() => HandleDelete(message.$id)}
+                className="bg-none text-xl text-white hover:text-red-600"
+              >
+                <FontAwesomeIcon icon={faTrash} />
+              </button>
             </div>
-            <button  onClick={()=>HandleDelete(message.$id)} className="bg-none text-xl text-white hover:text-red-600">
-              <FontAwesomeIcon icon={faTrash} />
-            </button>
-          </div>
-        ))}
+          ))}
         </div>
         <form
           className="message-ip sticky bottom-0 bg-slate-900 px-5 py-5 w-full flex justify-between items-center text-white rounded-2xl text-xl"
